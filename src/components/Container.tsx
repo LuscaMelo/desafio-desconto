fcc1b99670a2116aa08a2a32bc6578b61c4b095a
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-29 14:58:50 -0300

    feat: filter buttons implement

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,6 +5,7 @@ import { MdOutlineArrowForwardIos } from 'react-icons/md'
 import cars from '../../public/api/cars.json'
 import { useEffect, useRef, useState } from 'react'
 import { CarouselDot } from './CarouselDot'
+import { FilterButton } from './FilterButton'
 
 interface iCar {
     id: string
@@ -14,6 +15,13 @@ interface iCar {
     imageUrl: string
 }
 
+const filters = [
+    { filterName: 'All Cars', id: 'all', active: true },
+    { filterName: 'SUV', id: 'suv', active: false },
+    { filterName: 'Sedan', id: 'sedan', active: false },
+    { filterName: 'Estate', id: 'estate', active: false }
+]
+
 export const Container = () => {
 
     const slideCounter = 373
@@ -21,6 +29,9 @@ export const Container = () => {
     const [slide, setSlide] = useState(0)
     const [width, setWidth] = useState(0)
     const [dotControl, setDotControl] = useState(0)
+    const [isBtnActive, setIsBtnActive] = useState(false)
+    const [carList, setCarList] = useState(cars)
+
     const refContainer = useRef<null | HTMLDivElement>(null);
 
     useEffect(() => {
@@ -28,6 +39,8 @@ export const Container = () => {
         const offsetWidth: any = refContainer.current?.offsetWidth
         setWidth(scrollWidth - offsetWidth)
     }, [setWidth])
+
+    useEffect(() => { setIsBtnActive(false) }, [isBtnActive])
 
     //Click on next button - Desktop
     const nextCard = () => {
@@ -55,16 +68,45 @@ export const Container = () => {
         id > width ? setSlide(width) : null
     }
 
+    //On filter Button Click
+    const handleActive = (id: string) => {
+        // Set button active
+        filters.map(filter => filter.id == id ? filter.active = true : filter.active = false)
+        setIsBtnActive(true)
+
+        //Filter cars
+        const filteredCars = cars.filter((car) => car.bodyType == id)
+        id == 'all' ? setCarList(cars) : setCarList(filteredCars)
+        setDotControl(0)
+        setSlide(0)
+    }
+
     return (
         <div>
-            <div className="container max-w-[1300px] mx-auto mt-40 overflow-hidden">
+            <div className="container max-w-[1300px] mx-auto mt-[4.5rem] overflow-hidden">
+
+                <div className='flex flex-col md:flex-row md:items-center px-5 gap-3 mb-10'>
+                    <span className='font-bold text-sm text-gray-800'>Sort by:</span>
+                    <div className='flex gap-1'>
+                        {
+                            filters.map(filter => (
+                                <div key={filter.id}>
+                                    <FilterButton id={filter.id} isActive={filter.active ? true : false} setActive={handleActive}>
+                                        {filter.filterName}
+                                    </FilterButton>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+
                 <div
                     ref={refContainer}
                     className="flex gap-5 w-full px-5"
                     style={{ transform: `translateX(-${slide}px)`, transition: 'ease 1s' }}
                 >
                     {
-                        cars.map((car: iCar) => (
+                        carList.map((car: iCar) => (
                             <CarCard
                                 key={car.id}
                                 bodyType={car.bodyType}
@@ -95,7 +137,7 @@ export const Container = () => {
             </div>
             <div className='flex justify-center items-center gap-3 mt-10 xl:hidden'>
                 {
-                    cars.map((car, index) => (
+                    carList.map((car, index) => (
                         <div key={index}>
                             <CarouselDot id={index} onSlide={handleSlide} isActive={index == dotControl ? true : false} />
                         </div>
---
+++ src/components/FilterButton.tsx
@@ -0,0 +1,17 @@
+interface iButtonProps {
+    id: string
+    children: string
+    isActive: boolean
+    setActive: Function
+}
+
+export const FilterButton = ({ children, isActive, setActive, id }: iButtonProps) => {
+    return (
+        <button className={`border border-gray-600 font-semibold text-sm md:text-md py-1 px-4 rounded-full shadow-md scale-95 duration-300 hover:scale-100
+         ${isActive ? 'bg-blue-700 border-blue-700 text-white' : ''}`}
+            onClick={() => setActive(id)}
+        >
+            {children}
+        </button>
+    )
+}
