76874c64e199845a4580b4032d4273818865332a
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-06-29 16:03:45 -0300

    fix: border of FilterButton component

--- src/components/FilterButton.tsx
+++ src/components/FilterButton.tsx
@@ -7,8 +7,8 @@ interface iButtonProps {
 
 export const FilterButton = ({ children, isActive, setActive, id }: iButtonProps) => {
     return (
-        <button className={`border border-gray-600 font-semibold text-sm md:text-md py-1 px-4 rounded-full shadow-md scale-95 duration-300 hover:scale-100
-         ${isActive ? 'bg-blue-700 border-blue-700 text-white' : ''}`}
+        <button className={`border font-semibold text-sm md:text-md py-1 px-4 rounded-full shadow-md scale-95 duration-300 hover:scale-100
+         ${isActive ? 'bg-blue-700 border-blue-700 text-white' : 'border-gray-600'}`}
             onClick={() => setActive(id)}
         >
             {children}
